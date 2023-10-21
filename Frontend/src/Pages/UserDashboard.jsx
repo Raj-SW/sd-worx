a94398ab58c555d1b193619cf7c4188a0521bb6d
chavi <surujbhalichavi@gmail.com>
2023-10-21 12:54:22 +0400

    load trips

--- Frontend/src/Pages/UserDashboard.jsx
+++ Frontend/src/Pages/UserDashboard.jsx
@@ -1,20 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./UserDashboard.css";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import InputGroup from "react-bootstrap/InputGroup";
+import axios from 'axios';
+import { Avatar, List, Skeleton, DatePicker, Space, Modal, notification } from 'antd';
 
 export const CreateTrip = () => {
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
   const [seats, setSeats] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission, e.g., sending data to a backend API
-  };
+  const [trips, setTrips] = useState([]);
 
   // For handling date
   const [selectedDate, setSelectedDate] = useState("");
@@ -36,6 +34,94 @@ export const CreateTrip = () => {
   const handleNumberChange = (e) => {
     setSelectedNumber(e.target.value);
   };
+  const [api, contextHolder] = notification.useNotification();
+
+
+  const handleCreateTrip = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      const queryParams = {
+        auth: {
+          app_token: process.env.REACT_APP_TOKEN
+        },
+        data: {
+          driver: JSON.parse(atob(localStorage.getItem('token').split('.')[1])).id,
+          date: selectedDate,
+          departure_time: selectedTime,
+          available_seats: selectedNumber,
+          available_for_carpool: "true"
+        }
+      }
+      const YOUR_TOKEN = localStorage.getItem('token');
+
+      const response = await axios.post('http://localhost:3550/v1/trip/new', queryParams, {
+        headers: {
+          'Authorization': `Bearer ${YOUR_TOKEN}`
+        }
+      });
+
+
+      // Handle the response as needed
+      console.log('Server response:', response.data);
+
+      notification.success({
+        message: 'Success',
+        description: 'Trip created successfully.',
+      });
+      openNotification();
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const openNotification = () => {
+    api.open({
+      message: 'Success!!!',
+      description:
+        'you will be carpooling with this driver.',
+      duration: 5,
+      type: 'success',
+    });
+  };
+
+  const loadCreatedTrips = () => {
+
+    try {
+
+      const queryParams = {
+        auth: {
+          app_token: process.env.REACT_APP_TOKEN
+        },
+        data: {
+          driver_id: JSON.parse(atob(localStorage.getItem('token').split('.')[1])).id,
+        }
+      }
+      const YOUR_TOKEN = localStorage.getItem('token');
+
+      axios.post('http://localhost:3550/v1/trip/search', queryParams, {
+        headers: {
+          'Authorization': `Bearer ${YOUR_TOKEN}`
+        }
+      }).then(response => {
+        // Handle the response as needed
+        console.log('Server response:', response.data);
+        setTrips(response.data.data)
+      })
+
+
+
+
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+
+  useEffect(() => {
+    loadCreatedTrips()
+  }, [])
 
   return (
     <div className="CreateTrip-wrapper">
@@ -71,52 +157,35 @@ export const CreateTrip = () => {
               />
             </Col>
             <Col xs="auto" className="px-3">
-            <div className="UDB-btn">
-                <button className="vvd"><span>Create Trip</span></button>
-                </div>
+              <div className="UDB-btn">
+                <button className="vvd" onClick={handleCreateTrip}><span>Create Trip</span></button>
+              </div>
             </Col>
           </Row>
         </Form>
       </div>
-      <div className="trip-table-container">
-      <table id="table-trips">
-        <thead>
-          <tr>
-            <th>Column 1</th>
-            <th>Column 2</th>
-            <th>Column 3</th>
-            <th>Column 4</th>
 
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Row 1, Cell 1</td>
-            <td>Row 1, Cell 2</td>
-            <td>Row 1, Cell 3</td>
-            <td>Row 1, Cell 3</td>
-          </tr>
-          <tr>
-            <td>Row 2, Cell 1</td>
-            <td>Row 2, Cell 2</td>
-            <td>Row 2, Cell 3</td>
-            <td>Row 2, Cell 3</td>
-          </tr>
-          <tr>
-            <td>Row 3, Cell 1</td>
-            <td>Row 3, Cell 2</td>
-            <td>Row 3, Cell 3</td>
-            <td>Row 3, Cell 3</td>
-          </tr>
-          <tr>
-            <td>Row 4, Cell 1</td>
-            <td>Row 4, Cell 2</td>
-            <td>Row 4, Cell 3</td>
-            <td>Row 4, Cell 3</td>
-          </tr>
-        </tbody>
-      </table>
-      </div>
+
+      <List
+        style={{
+          width: "900px",
+          margin: "auto"
+        }}
+        itemLayout="horizontal"
+        dataSource={trips}
+        renderItem={(item, index) => (
+          <List.Item actions={[<Button danger key={item.driver.id} type='text'>Driver's Route</Button>, <Button ghost type='primary' key="list-loadmore-more" >Share Car</Button>]}>
+            <List.Item.Meta
+              avatar={<Avatar src={`https://th.bing.com/th/id/OIP.AkKR5-4AJhHTNNDMp0NxvQAAAA?pid=ImgDet&rs=1`} />}
+              title={item.driver.name}
+              description={<span dangerouslySetInnerHTML={{
+                __html: `Address: ${item.driver.address} <br>Drive Time on Road: ${item.departure_time} <br> Pickup Time: ${item.estimated_pickup_time} <br> Seats Left: ${item.available_seats
+                  }`
+              }} />}
+            />
+          </List.Item>
+        )}
+      />
     </div>
   );
 };
